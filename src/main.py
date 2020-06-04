9c8a5c50409ce57f192e56209109454250551ca1
tokudaek <tokudaek@gmail.com>
2020-06-04 10:30:22 -0300

    working version

--- src/main.py
+++ src/main.py
@@ -18,12 +18,9 @@ import igraph
 import matplotlib.collections as mc
 import pickle
 import pandas as pd
+from scipy.spatial import cKDTree
 
 HOME = os.getenv('HOME')
-sys.path.append(pjoin(HOME, 'projects/cityblocks/'))
-import src.run_realcities as cityblocks
-
-
 
 #############################################################
 def info(*args):
@@ -31,49 +28,65 @@ def info(*args):
     print(pref, *args, file=sys.stdout)
 
 ##########################################################
-def main():
+def parse_graphml(graphmlpath, pklpath, samplerad=-1):
+    """Read graphml file to igraph object and dump it to @pklpath
+    Receives the input path @graphmlpath and dump to @pklpath
+    """
     info(inspect.stack()[0][3] + '()')
-    t0 = time.time()
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--outdir', default='/tmp/out/', help='Output directory')
-    args = parser.parse_args()
 
-    if not os.path.isdir(args.outdir): os.mkdir(args.outdir)
+    if os.path.exists(pklpath):
+        info('Loading existing file: {}'.format(pklpath))
+        return pickle.load(open(pklpath, 'rb'))
 
-    # graphsdir = '/home/frodo/results/graffiti/20200202-types/'
-    # graphmlpath = '/home/frodo/results/graffiti/20200202-types/20200221-citysp.graphml'
-    # g = igraph.Graph.Read(graphmlpath)
-    # g.simplify()
-    # g.to_undirected()
-    # coords = [(float(x), -float(y)) for x, y in zip(g.vs['x'], g.vs['y'])]
-    # pklpath = '/tmp/citysp.pkl'
-    # pickle.dump(g, open(pklpath, 'wb'))
+    g = igraph.Graph.Read(graphmlpath)
+    g.simplify(); g.to_undirected()
+    g = sample_circle_from_graph(g, samplerad)
+    pickle.dump(g, open(pklpath, 'wb'))
+    return g
 
-    pklpath = pjoin(HOME, 'temp/citysp.pkl')
-    g = pickle.load(open(pklpath, 'rb'))
-    info('g.vcount():{}'.format(g.vcount()))
-    g.delete_vertices(np.arange(100000))
+##########################################################
+def sample_circle_from_graph(g, radius):
+    """Short description
+    Receives g, pklpath, overwrite=True and returns a ret
+    """
+    info(inspect.stack()[0][3] + '()')
+    if radius < 0: return g
+    coords = [(float(x), float(y)) for x, y in zip(g.vs['x'], g.vs['y'])]
+    c0 = coords[np.random.randint(g.vcount())]
+    ids = get_points_inside_region(coords, c0, radius)
+    todel = np.ones(g.vcount(), bool)
+    todel[ids] = False
+    g.delete_vertices(np.where(todel == True)[0])
+    return g
 
-    # breakpoint()
-    
-    norig = g.ecount()
+##########################################################
+def get_points_inside_region(coords, c0, radius):
+    """Get points from @df within circle of center @c0 and @radius
+    """
+    info(inspect.stack()[0][3] + '()')
+    kdtree = cKDTree(coords)
+    inds = kdtree.query_ball_point(c0, radius)
+    return sorted(inds)
+
+##########################################################
+def analyze_random_increment_of_edges(g, nnewedges, outcsv):
+    """Analyze random increment of n edges to @g for each value n in @nnewedges
+    """
+    info(inspect.stack()[0][3] + '()')
     acc = 0
     values = []
-    for m in [10, 100, 1000]:
-    # for x in [10, 100, 1000, 10000]:
-        info('m:{}'.format(m))
-        nnew = m - acc
+
+    for n in [0] + nnewedges:
+        info('Adding {} edges'.format(n))
+        nnew = n - acc
         for i in range(nnew):
             s, t = np.random.choice(g.vcount(), size=2, replace=False)
             g.add_edge(s, t)
-        values.append([m, g.average_path_length()])
-        acc += m
+        values.append([n, g.average_path_length()])
+        acc += n
 
     df = pd.DataFrame(values, columns=['nnewedges', 'avgpathlen'])
-    df.to_csv(pjoin(args.outdir, 'lengths.csv'))
-    
-    # plot_map(g, args.outdir)
-    info('Elapsed time:{}'.format(time.time()-t0))
+    df.to_csv(outcsv, index=False)
 
 ##########################################################
 def plot_map(g, outdir):
@@ -101,6 +114,31 @@ def plot_map(g, outdir):
 
     plt.savefig(pjoin(outdir, 'map.pdf'))
 
+##########################################################
+def main():
+    info(inspect.stack()[0][3] + '()')
+    t0 = time.time()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--outdir', default='/tmp/out/', help='Output directory')
+    parser.add_argument('--samplerad', default=-1, type=float, help='Sample radius')
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.outdir): os.mkdir(args.outdir)
+
+    np.random.seed(0)
+    graphmlpath = 'data/map.graphml'
+    pklpath = 'data/map.pkl'
+    outcsv = pjoin(args.outdir, 'shortest.csv')
+    nnewedges = [10]
+
+    g = parse_graphml(graphmlpath, pklpath, samplerad=args.samplerad)
+    info('nvertices: {}'.format(g.vcount()))
+    info('nedges: {}'.format(g.ecount()))
+
+    analyze_random_increment_of_edges(g, nnewedges, outcsv)
+    
+    plot_map(g, args.outdir)
+    info('Elapsed time:{}'.format(time.time()-t0))
 
 ##########################################################
 if __name__ == "__main__":
