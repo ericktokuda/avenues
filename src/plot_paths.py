05ac117f71fe92b46bd14ca2d248c9b4f6948c28
tokudaek <tokudaek@gmail.com>
2020-10-01 13:55:39 -0300

    Update plots

--- src/plot_paths.py
+++ src/plot_paths.py
@@ -23,8 +23,8 @@ def plot_global(df, outdir):
     fig, axs = plt.subplots(nrows, ncols, figsize=(ncols*figscale, nrows*figscale))
 
     y = df.g_pathlen_mean
-    # axs.scatter(list(range(len(y))), y, label='global')
     axs.errorbar(list(range(len(y))), y, yerr=df.g_pathlen_std)
+    axs.set_ylim(0, np.max(y)*1.2)
     plt.savefig(pjoin(outdir, 'global.png'))
 
 ##########################################################
@@ -42,12 +42,12 @@ def plot_local_individually(df, outdir):
         axs.plot(list(range(len(df))), y, label=col)
         cols.append(col)
 
-    # plt.legend()
+    axs.set_ylabel('Length')
     axs.set_title('Individual path length for {} regions'.format(len(cols)))
     plt.savefig(pjoin(outdir, 'local_individually.png'))
 
 ##########################################################
-def plot_local_all(df, outdir):
+def plot_local_mean(df, outdir):
     """Plot each line individually """
     info(inspect.stack()[0][3] + '()')
 
@@ -63,29 +63,26 @@ def plot_local_all(df, outdir):
 
     axs.errorbar(list(range(len(df))), np.mean(vals, axis=1),
             yerr=np.std(vals, axis=1))
-    axs.set_title('Mean Path lengths (across {} regions)'.format(len(cols)))
-    # plt.legend()
-    plt.savefig(pjoin(outdir, 'local_all.png'))
+    axs.set_ylabel('Length')
+    axs.set_title('Average path lengths (across {} regions)'.format(len(cols)))
+    plt.savefig(pjoin(outdir, 'local_mean.png'))
 
 ##########################################################
-def plot_corr_all(df, outdir):
+def plot_corr_all(df, localfeats, outdir):
     """Plot correlation between independent variables """
     info(inspect.stack()[0][3] + '()')
 
     # data = dict()
     data = pd.DataFrame()
 
-
     data['g_nbridges'] = df.nbridges
     data['g_naccess'] = df.naccess
-    data['g_pathlen_mean'] = df.g_pathlenmean
-    data['g_pathlen_std'] = df.g_pathlenstd
+    data['g_pathlen_mean'] = df.g_pathlen_mean
+    data['g_pathlen_std'] = df.g_pathlen_std
     # data['g_betwmean'] = df.g_betwmean
     # data['g_betwstd'] = df.g_betwstd
 
-    localfeat =  ['pathlen', 'degree', 'divers', 'assort', 'clucoeff', 'closeness']
-
-    for feat in localfeat:
+    for feat in localfeats:
         cols = []
         for col in df.columns:
             if col.startswith(feat + '_'): cols.append(col)
@@ -107,17 +104,46 @@ def plot_corr_all(df, outdir):
     ax.set_yticklabels(data.columns)
     plt.savefig(pjoin(outdir, 'heatmap.png'))
 
+##########################################################
+def plot_hists(df, localfeats, outdir):
+    """Plot histogram of the features"""
+    info(inspect.stack()[0][3] + '()')
+    m = len(df)
+    for col1 in localfeats[1:]:
+        x = np.zeros(m, dtype=float)
+        for k in range(30):
+            x += df['{}_{:03d}'.format(col1, k)].values
 
-    # cols = data.columns
-    # for i, col1 in enumerate(cols):
-        # for col2 in cols[i+1:]:
-            # nrows = 1;  ncols = 1; figscale = 8
-            # fig, axs = plt.subplots(nrows, ncols,
-                        # figsize=(ncols*figscale, nrows*figscale))
-            # axs.scatter(data[col1], data[col2])
-            # plt.tight_layout()
-            # plt.savefig(pjoin(outdir, '{}_{}.png'.format(col1, col2)))
-            # plt.close()
+        nrows = 1;  ncols = 1; figscale = 8
+        fig, axs = plt.subplots(nrows, ncols,
+                    figsize=(ncols*figscale, nrows*figscale))
+        axs.hist(x)
+        axs.set_xlabel(col1)
+        plt.tight_layout()
+        plt.savefig(pjoin(outdir, 'hist_{}.png'.format(col1)))
+        plt.close()
+
+def plot_pairwise_points(df, localfeats, outdir):
+    m = len(df)
+    for col1 in localfeats[1:]:
+        x = np.zeros(m, dtype=float)
+        for k in range(30):
+            x += df['{}_{:03d}'.format(col1, k)].values
+        for col2 in ['pathlen']:
+            y = np.zeros(m, dtype=float)
+            for k in range(30):
+                y += df['{}_{:03d}'.format(col2, k)].values
+
+        nrows = 1;  ncols = 1; figscale = 8
+        fig, axs = plt.subplots(nrows, ncols,
+                    figsize=(ncols*figscale, nrows*figscale))
+        axs.scatter(x, y)
+        axs.set_xlabel(col1)
+        axs.set_ylabel('Pathlen')
+        plt.tight_layout()
+        plt.savefig(pjoin(args.outdir, '{}_{}.png'.format(col1, col2)))
+        plt.close()
+
 ##########################################################
 def main():
     info(inspect.stack()[0][3] + '()')
@@ -131,45 +157,14 @@ def main():
     readmepath = create_readme(sys.argv, args.outdir)
 
     df = pd.read_csv(args.results)
+    localfeats = ['pathlen', 'degree', 'betwv', 'divers', 'clucoeff', 'closeness']
 
     plot_global(df, args.outdir)
     plot_local_individually(df, args.outdir)
-    plot_local_all(df, args.outdir)
+    plot_local_mean(df, args.outdir)
+    plot_corr_all(df, localfeats, args.outdir)
+    plot_hists(df, localfeats, args.outdir)
 
-    cols = ['pathlen', 'degree', 'betwv', 'divers', 'clucoeff', 'closeness']
-    m = len(df)
-    for i, col1 in enumerate(cols):
-        x = np.zeros(m, dtype=float)
-        for k in range(30):
-            x += df['{}_{:03d}'.format(col1, k)].values
-
-        nstr = '{:03d}'.format(i) # plot histogram
-        nrows = 1;  ncols = 1; figscale = 8
-        fig, axs = plt.subplots(nrows, ncols,
-                    figsize=(ncols*figscale, nrows*figscale))
-        axs.hist(x)
-        plt.tight_layout()
-        plt.savefig(pjoin(args.outdir, 'hist_{}.png'.format(col1)))
-        plt.close()
-
-        # for col2 in cols[i+1:]:
-        for col2 in ['pathlen']:
-            y = np.zeros(m, dtype=float)
-            for k in range(30):
-                y += df['{}_{:03d}'.format(col2, k)].values
-
-
-        nstr = '{:03d}'.format(i)
-        nrows = 1;  ncols = 1; figscale = 8
-        fig, axs = plt.subplots(nrows, ncols,
-                    figsize=(ncols*figscale, nrows*figscale))
-        axs.scatter(x, y)
-        plt.tight_layout()
-        plt.savefig(pjoin(args.outdir, '{}_{}.png'.format(col1, col2)))
-        plt.close()
-
-
-        # plt.errorbar(list(range(len(df))), avgplennorm, yerr=df.stdpathlen,
     info('Elapsed time:{}'.format(time.time()-t0))
     info('Output generated in {}'.format(args.outdir))
 
