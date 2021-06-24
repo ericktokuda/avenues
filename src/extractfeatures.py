9235b96bf15eeb055753481e9f0b8833a308f08d
tokudaek <tokudaek@gmail.com>
2021-06-24 09:01:53 -0300

    First version of the features

--- src/extractfeatures.py
+++ src/extractfeatures.py
@@ -18,6 +18,9 @@ from lacuncaller import calculate_lacunarity
 
 ##########################################################
 def extract_degree_feats(g):
+    """Calculate features related to the degree distribution. Extract the
+    mean, std, fraction of vertices with degrees 3, 4 and 5."""
+    info(inspect.stack()[0][3] + '()')
     degrees = np.array(g.degree())
     n = len(degrees)
     degrmean = np.mean(degrees)
@@ -33,12 +36,10 @@ def extract_degree_feats(g):
             idx = ids[0]
             fr[k] = ucounts[idx] / n
 
-    city = os.path.splitext(f)[0]
-
     return [degrmean, degrstd, fr[3], fr[4], fr[5]]
 
 ##########################################################
-def extract_angle_entropy(g, bins):
+def calculate_angle_entropy(g, bins):
     """Extract orientation from each edge. We adopt the angle 0 from a given point as the horizontal vector pointing to the right. Angles increase increase anti-clockwise.
     """
     coords = np.array([(x, y) for x, y in zip(g.vs['x'], g.vs['y'])])
@@ -54,19 +55,19 @@ def extract_angle_entropy(g, bins):
     return -(distr*np.log(np.abs(distr))).sum()
 
 ##########################################################
-def main():
+def calculate_accessib_feats(accpath):
+    """Calculate accessibility features"""
     info(inspect.stack()[0][3] + '()')
-    t0 = time.time()
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--graphsdir', required=True, help='Path to the dir with graphs inside')
-    parser.add_argument('--outdir', default='/tmp/out/', help='Output directory')
-    args = parser.parse_args()
+    accs = np.loadtxt(accpath)
+    return [np.mean(accs), np.std(accs)]
 
-    os.makedirs(args.outdir, exist_ok=True)
-    readmepath = create_readme(sys.argv, args.outdir)
-    csvpath = pjoin(args.outdir, 'stats.csv')
+##########################################################
+def main(graphsdir, accessibdir, outdir):
+    """Main function"""
+    info(inspect.stack()[0][3] + '()')
+    outpath = pjoin(outdir, 'feats.csv')
 
-    files = sorted(os.listdir(args.graphsdir))
+    files = sorted(os.listdir(graphsdir))
     data = []
 
     # Angle feature params
@@ -79,31 +80,52 @@ def main():
 
     # Lacunarity params
     px_per_km = 100 # To obtain per meter, divide by 1000
-    max_radius = 100
-    delta_radius = 5
+    max_radius = 52
+    delta_radius = 10
 
+    accstep = 5
+    accsuf = '_undirected_acc{:02d}.txt'.format(accstep)
+
+    data = []
     for f in files:
-        graphml = pjoin(args.graphsdir, f)
+        graphml = pjoin(graphsdir, f)
         if not graphml.endswith('.graphml'): continue
-        info('f:{}'.format(f))
+        city = f.replace('.graphml', '')
+        info('{}'.format(f))
+        accpath = pjoin(accessibdir, city + accsuf)
+        if not os.path.exists(accpath):
+            info('Accessib file {} does not exist. Skipping'.format(accpath))
+            continue
         g = graph.simplify_graphml(graphml)
-        # degrfeats = extract_degree_feats(g)
-        entropyan = extract_angle_entropy(g, bins)
+        degrfeats = extract_degree_feats(g)
+        entropyan = calculate_angle_entropy(g, bins)
         entropyvx = calculate_rastered_graph_entropy(graphml, sigma, max_ratio,
                                                     min_width)
         radii, lacun = calculate_lacunarity(graphml, max_radius, delta_radius,
                                             px_per_km)
-        breakpoint()
-        # row = angfeats + degrfeats
+        accfeats = calculate_accessib_feats(accpath)
+        row = [city] + degrfeats + [entropyan, entropyvx] + list(lacun) + accfeats
+        data.append(row)
 
-    # data.append([city, degrmean, degrstd, fr[3], fr[4], fr[5]])
-    df = pd.DataFrame(data, columns=['city', 'degrmean', 'degrstd',
-                                     'frac3', 'frac4', 'frac5'])
-    df.to_csv(pjoin(args.outdir, 'feats.csv'), index=False)
-
-    info('Elapsed time:{}'.format(time.time()-t0))
-    info('Output generated in {}'.format(args.outdir))
+    lacuncols = ['lacun{}'.format(r) for r in radii]
+    cols = ['city', 'degrmean', 'degrstd', 'degr3', 'degr4', 'degr5',
+            'entropyang', 'entropyvx'] + lacuncols + ['accmean', 'accstd']
+    df = pd.DataFrame(data, columns=cols)
+    df.to_csv(outpath, index=False)
 
 ##########################################################
 if __name__ == "__main__":
-    main()
+    t0 = time.time()
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--graphsdir', required=True, help='Path to the dir with graphs inside')
+    parser.add_argument('--accessibdir', required=True, help='Path to the dir with accessibilities inside')
+    parser.add_argument('--outdir', default='/tmp/out/', help='Output directory')
+    args = parser.parse_args()
+
+    os.makedirs(args.outdir, exist_ok=True)
+    readmepath = create_readme(sys.argv, args.outdir)
+
+    main(args.graphsdir, args.accessibdir, args.outdir)
+
+    info('Elapsed time:{}'.format(time.time()-t0))
+    info('Output generated in {}'.format(args.outdir))
